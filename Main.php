aee2a8c963cf90b1b9d3093934f3116f25b91450
Marcus Povey <marcus@marcus-povey.co.uk>
2018-03-28 19:17:42 +0100

    Updating IndiePub project from core

--- Main.php
+++ Main.php
@@ -2,18 +2,87 @@
 
     namespace IdnoPlugins\IndiePub {
 
-        class Main extends \Idno\Common\Plugin {
-            function registerPages() {
+        use IdnoPlugins\IndiePub\Pages\IndieAuth\Token;
 
-                \Idno\Core\site()->addPageHandler('/indieauth/callback/?', '\IdnoPlugins\IndiePub\Pages\IndieAuth\Callback',true);
-                \Idno\Core\site()->addPageHandler('/indieauth/token/?', '\IdnoPlugins\IndiePub\Pages\IndieAuth\Token',true);
-                \Idno\Core\site()->addPageHandler('/micropub/endpoint/?', '\IdnoPlugins\IndiePub\Pages\MicroPub\Endpoint',true);
-                \Idno\Core\site()->template()->extendTemplate('shell/head','indiepub/shell/head');
+        class Main extends \Idno\Common\Plugin
+        {
 
-                header('Link: <https://indieauth.com/auth>; rel="authorization_endpoint"');
-                header('Link: <'.\Idno\Core\site()->config()->getURL().'indieauth/token>; rel="token_endpoint"');
-                header('Link: <'.\Idno\Core\site()->config()->getURL().'micropub/endpoint>; rel="micropub"');
+            function registerTranslations() {
+
+                \Idno\Core\Idno::site()->language()->register(
+                    new \Idno\Core\GetTextTranslation(
+                        'indiepub', dirname(__FILE__) . '/languages/'
+                    )
+                );
             }
+
+            function registerEventHooks()
+            {
+                \Idno\Core\site()->addEventHook('user/auth/request', function (\Idno\Core\Event $event) {
+                    if ($user = \IdnoPlugins\IndiePub\Main::authenticate()) {
+                        $event->setResponse($user);
+                    }
+                });
+            }
+
+            function registerPages()
+            {
+                \Idno\Core\Idno::site()->addPageHandler('/indieauth/auth/?', '\IdnoPlugins\IndiePub\Pages\IndieAuth\Auth', true);
+                \Idno\Core\Idno::site()->addPageHandler('/indieauth/approve/?', '\IdnoPlugins\IndiePub\Pages\IndieAuth\Approve', true);
+                \Idno\Core\Idno::site()->addPageHandler('/indieauth/token/?', '\IdnoPlugins\IndiePub\Pages\IndieAuth\Token', true);
+                \Idno\Core\Idno::site()->addPageHandler('/micropub/endpoint/?', '\IdnoPlugins\IndiePub\Pages\MicroPub\Endpoint', true);
+                \Idno\Core\Idno::site()->addPageHandler('/account/indiepub/?', '\IdnoPlugins\IndiePub\Pages\Account');
+                \Idno\Core\Idno::site()->addPageHandler('/account/indiepub/revoke/?', '\IdnoPlugins\IndiePub\Pages\Revoke');
+
+                \Idno\Core\Idno::site()->template()->extendTemplate('shell/head', 'indiepub/shell/head');
+                \Idno\Core\Idno::site()->template()->extendTemplate('account/menu/items', 'account/menu/items/indiepub');
+
+                header('Link: <' . \Idno\Core\Idno::site()->config()->getURL() . 'indieauth/auth>; rel="authorization_endpoint"', false);
+                header('Link: <' . \Idno\Core\Idno::site()->config()->getURL() . 'indieauth/token>; rel="token_endpoint"', false);
+                header('Link: <' . \Idno\Core\Idno::site()->config()->getURL() . 'micropub/endpoint>; rel="micropub"', false);
+            }
+
+            /**
+             * Check that this token is either a user token or the
+             * site's API token, and auth the current request for that user if so.
+             *
+             * @return \Idno\Entities\User user on success
+             */
+            private static function authenticate()
+            {
+                $access_token = \Idno\Core\Input::getInput('access_token');
+                $headers      = \Idno\Common\Page::getallheaders();
+                if (!empty($headers['Authorization'])) {
+                    $token = $headers['Authorization'];
+                    $token = trim(str_replace('Bearer', '', $token));
+                } else if ($token = \Idno\Core\Input::getInput('access_token')) {
+                    $token = trim($token);
+                }
+
+                if (!empty($token)) {
+
+                    $found = Token::findUserForToken($token);
+                    if (!empty($found)) {
+                        \Idno\Core\Idno::site()->session()->setIsAPIRequest(true);
+                        $user = $found['user'];
+                        \Idno\Core\Idno::site()->session()->refreshSessionUser($user);
+
+                        return $user;
+                    }
+                    $user = \Idno\Entities\User::getOne(array('admin' => true));
+                    if ($user)
+                        if ($token == $user->getAPIkey()) {
+                            \Idno\Core\Idno::site()->session()->setIsAPIRequest(true);
+                            \Idno\Core\Idno::site()->session()->refreshSessionUser($user);
+
+                            return $user;
+                        }
+                }
+
+                return false;
+            }
+
+
         }
 
     }
---
+++ templates/default/account/menu/items/indiepub.tpl.php
@@ -0,0 +1,7 @@
+<?php
+$baseURL = \Idno\Core\Idno::site()->config()->getDisplayURL();
+?>
+
+<li>
+  <a href="<?= $baseURL ?>account/indiepub/"><?= \Idno\Core\Idno::site()->language()->_('IndiePub'); ?></a>
+</li>
